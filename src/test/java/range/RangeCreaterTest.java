5703e05240efe02e9c7faec3a54391c9542a8e4d
Keerthana Kuppuchamy (SX/EAB1) <feu1cob@bosch.com>
2023-01-25 16:00:17 +0530

    code change

--- src/test/java/range/RangeCreaterTest.java
+++ src/test/java/range/RangeCreaterTest.java
@@ -18,15 +18,22 @@ public class RangeCreaterTest {
 		Map<String, Integer> map = RangeCreater.generateMapForReadingAndRange(values);
 		assertTrue(map.containsKey("4-5"));
 		assertEquals(new Integer(2), map.get("4-5"));
-	}
 
-	@Test
-	public void testRangeCreatorComplex() {
-		List<Integer> values = new ArrayList<Integer>(Arrays.asList(1, 2, 4, 5, 6));
-		Map<String, Integer> map = RangeCreater.generateMapForReadingAndRange(values);
+		values = new ArrayList<Integer>(Arrays.asList(1, 2, 4, 5, 6));
+		map = RangeCreater.generateMapForReadingAndRange(values);
 		assertTrue(map.containsKey("4-6"));
 		assertTrue(map.containsKey("1-2"));
 		assertEquals(new Integer(2), map.get("1-2"));
 		assertEquals(new Integer(3), map.get("4-6"));
+
+		values = new ArrayList<Integer>(Arrays.asList(1, 1, 1, 2, 2));
+		map = RangeCreater.generateMapForReadingAndRange(values);
+		assertTrue(map.containsKey("1-2"));
+		assertEquals(new Integer(5), map.get("1-2"));
+
+		values = new ArrayList<Integer>();
+		map = RangeCreater.generateMapForReadingAndRange(values);
+		assertTrue(map.isEmpty());
+
 	}
 }
